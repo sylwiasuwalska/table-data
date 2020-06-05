1791fd6fce43aadffc70f5c9862c6f577eb38ba6
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-06-05 12:17:04 +0200

    feat: add basic view of company

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,21 +1,41 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { stateContext } from "./Store";
 import { useHistory } from "react-router-dom";
 
 function Company(props) {
   useEffect(() => props.setActualLocation(props.history.location.pathname), []);
-  // const state = useContext(stateContext);
-  //
-  // const company = state.slice().filter(element=>(element.id)==props.match.params.id).map(()=> {
-  //
-  // })
-  //console.log(company)
+  const state = useContext(stateContext);
+
+  const company = state
+    .filter((element) => element.id == props.match.params.id)
+    .map((data, index) => {
+      const {
+        id,
+        name,
+        city,
+        totalIncome,
+        averageIncome,
+        lastMonthIncome,
+      } = data;
+
+      return (
+        <div key={index}>
+          <h2>Company name: {name}</h2>
+          <p key={index.id}>Company id: {id}</p>
+          <p key={index.city}>City: {city}</p>
+          <p key={index.totalIncome}>Total income: {totalIncome}</p>
+          <p key={index.averageIncome}>Average income: {averageIncome}</p>
+            <p key={index.lastMonthIncome}>Last month income: {lastMonthIncome}</p>
+        </div>
+      );
+    });
+  console.log(company);
   let history = useHistory();
 
   return (
     <>
-      <div>company</div>
-      <button onClick={() => history.goBack()}>take me back</button>
+      <button onClick={() => history.goBack()}> ← take me back</button>
+      <div>{company}</div>
     </>
   );
 }
