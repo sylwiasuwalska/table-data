30f237bb409104dc86a0841bbf369e3336146632
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-06-08 11:44:07 +0200

    Updates

---
+++ precache-manifest.4f146ba6daa6ce40f667235ca92a4bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623c48cacf8c87f2440d72bcc1b1ca81",
+    "url": "/table-data/index.html"
+  },
+  {
+    "revision": "63b83f0f07f77972025a",
+    "url": "/table-data/static/css/main.0799617c.chunk.css"
+  },
+  {
+    "revision": "b49086352c5d63cf4fa6",
+    "url": "/table-data/static/js/2.3cc78703.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/table-data/static/js/2.3cc78703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b83f0f07f77972025a",
+    "url": "/table-data/static/js/main.296dc906.chunk.js"
+  },
+  {
+    "revision": "059d953dc4b0b7b75c98",
+    "url": "/table-data/static/js/runtime-main.dc9e1931.js"
+  },
+  {
+    "revision": "90c2963bb398520769d9c74322d97fb0",
+    "url": "/table-data/static/media/ring.90c2963b.svg"
+  }
+]);
